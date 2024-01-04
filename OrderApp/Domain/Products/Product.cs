1993b41c3ace8ff8887a1306f725a5a60f0820c6
viniraf <vrmb.pro@gmail.com>
2024-01-04 15:14:54 -0300

    Added entity framework context and adjusted/configured products properties

--- OrderApp/Domain/Products/Product.cs
+++ OrderApp/Domain/Products/Product.cs
@@ -4,6 +4,8 @@ public class Product : Entity
 {
     public string Name { get; set; } = string.Empty;
 
+    public int CategoryId { get; set; }
+
     public string Description { get; set; } = string.Empty;
 
     public Category? Category { get; set; } 
---
+++ OrderApp/Infra/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApp.Domain.Products;
+
+namespace OrderApp.Infra.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name).IsRequired();
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(255);
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Name).IsRequired();
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<string>()
+            .HaveMaxLength(100);
+    }
+}
