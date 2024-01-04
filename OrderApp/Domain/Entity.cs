5c4336e47cfffd17f2337ebae88854efca93036a
viniraf <vrmb.pro@gmail.com>
2024-01-03 22:42:08 -0300

    Created domain folder and related classes

---
+++ OrderApp/Domain/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace OrderApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public DateTime CreatedOn { get; set; }
+
+    public string EditedBy { get; set; } = string.Empty;
+
+    public DateTime EditedOn { get; set; }
+}
