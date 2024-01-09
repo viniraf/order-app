289e874eac7f0bd2a3e78be4725500d793cea61d
viniraf <vrmb.pro@gmail.com>
2024-01-08 22:20:54 -0300

    Created employess folder in endpoints and added EmployeePost action

---
+++ OrderApp/Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,30 @@
+﻿using Flunt.Notifications;
+using Microsoft.AspNetCore.Identity;
+using OrderApp.Domain.Products;
+using OrderApp.Infra.Data;
+
+namespace OrderApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    public static string Template => "/employees";
+
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+
+    public static Delegate Handle => Action;
+
+    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    {
+        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+
+        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+
+        if (!result.Succeeded)
+        {
+            // TODO: Add problem details for result
+            return Results.BadRequest(result.Errors.First());
+        }
+
+        return Results.Created($"/employees/{user.Id}", $"Created Employee Id: {user.Id}");
+    }
+}
--- OrderApp/Program.cs
+++ OrderApp/Program.cs
@@ -1,6 +1,7 @@
 using DotNetEnv;
 using Microsoft.AspNetCore.Identity;
 using OrderApp.Endpoints.Categories;
+using OrderApp.Endpoints.Employees;
 using OrderApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,8 +17,14 @@ string connectionString = Env.GetString("DB_CONNECTION_STRING");
 
 builder.Services.AddSqlServer<ApplicationDbContext>(connectionString);
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequireLowercase = false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,4 +43,6 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGet.Template, CategoryGet.Methods, CategoryGet.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
+
 app.Run();
